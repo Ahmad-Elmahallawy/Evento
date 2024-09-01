5ce48d9d8a1bdcdeda3752efc9f46ccafded59da
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-09-01 12:42:42 -0400

    js to jsx files

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55070dd7a1f4ee83cb4adbcf0b3e6b82ed4ec19a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6426fb53ce867a2a37bdc72c0df58d8514cc3fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
